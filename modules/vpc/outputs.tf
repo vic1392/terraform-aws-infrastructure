94d6f414fd1fbed5f7ac6713d8e65c6b1e4c9193
vic1392 <vicky13921392@gmail.com>
2026-09-26 10:54:09 +0530

    Create outputs.tf

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  description = "The VPC ID"
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr_block" {
+  description = "The VPC CIDR block"
+  value       = aws_vpc.this.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.this.id
+}
+
+output "nat_gateway_ids" {
+  description = "List of NAT Gateway IDs"
+  value       = aws_nat_gateway.this[*].id
+}
+
+output "public_route_table_id" {
+  description = "Public route table ID"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_ids" {
+  description = "List of private route table IDs"
+  value       = aws_route_table.private[*].id
+}
